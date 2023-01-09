d835f91c091c62b75ae39a0d3f71c94718b9715d
caster25 <teerapst1236@gmail.com>
2023-01-09 12:15:47 +0700

    last01

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <body>
-    <form actiom="show.php">
+    <form action="show.php" method="post">
         What is your name: <input type="tetx" name="name"><br>
         what is your age:<input type ="tetx" name="age"><br>
         <input type= "submit" value="sudmit">
