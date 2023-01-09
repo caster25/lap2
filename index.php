8238a03fdafa328445602567355d10ce0d2f389c
caster25 <teerapst1236@gmail.com>
2023-01-09 12:08:53 +0700

    tast01

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form actiom="show.php">
+        What is your name: <input type="tetx" name="name"><br>
+        what is your age:<input type ="tetx" name="age"><br>
+        <input type= "submit" value="sudmit">
+    </form>
+</body>
+
+</html>
